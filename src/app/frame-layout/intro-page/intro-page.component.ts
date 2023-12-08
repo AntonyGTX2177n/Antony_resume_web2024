e15b4b6fdcf6d61d415e708054864119f916099f
Suresh-Antony~ <Antony@makolet.in>
2023-12-08 16:50:42 +0530

    intro page and other component set-up complete

---
+++ src/app/frame-layout/intro-page/intro-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-intro-page',
+  templateUrl: './intro-page.component.html',
+  styleUrls: ['./intro-page.component.css']
+})
+export class IntroPageComponent implements OnInit {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  toLogin() {
+    this.router.navigate(['login'])
+  }
+
+}
